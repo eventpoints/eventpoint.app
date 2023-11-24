d5c2de5c8f917d1a5707dce79bdce9614a3d751e
kez <kerrialbeckettnewham@gmail.com>
2023-11-24 09:03:29 +0100

    reinstall encore / remove all packages

--- assets/bootstrap.js
+++ assets/bootstrap.js
@@ -1,13 +1,13 @@
 import {startStimulusApp} from '@symfony/stimulus-bridge';
-import PasswordVisibility from 'stimulus-password-visibility'
-import Lightbox from 'stimulus-lightbox'
-import Calendar from 'stimulus-calendar'
+// import PasswordVisibility from 'stimulus-password-visibility'
+// import Lightbox from 'stimulus-lightbox'
+// import Calendar from 'stimulus-calendar'
 
 export const app = startStimulusApp(require.context(
     '@symfony/stimulus-bridge/lazy-controller-loader!./controllers',
     true,
     /\.[jt]sx?$/
 ));
-app.register('password-visibility', PasswordVisibility)
-app.register('lightbox', Lightbox)
-app.register('calendar', Calendar)
+// app.register('password-visibility', PasswordVisibility)
+// app.register('lightbox', Lightbox)
+// app.register('calendar', Calendar)
